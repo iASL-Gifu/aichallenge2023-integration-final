91827e107268430287f2f488b1a4a4043be95f52
knorrrr <kunotomoki61@gmail.com>
2023-11-04 16:45:54 +0900

    fix

--- scripts/iASL_launch_psim.sh
+++ scripts/iASL_launch_psim.sh
@@ -2,4 +2,4 @@
 
 source ./vars/vehicle.env
 
-ros2 launch autoware_launch planning_simulator.launch.xml map_path:=$MAP_PATH vehicle_model:=$VEHICLE_MODEL sensor_model:=awsim_sensor_kit
+ros2 launch autoware_launch planning_simulator.launch.xml map_path:=../map/AWSIM/ vehicle_model:=$VEHICLE_MODEL sensor_model:=awsim_sensor_kit
