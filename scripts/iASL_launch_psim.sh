d77de80ed5a3bd7817aeeac5633bf2763e58f086
knorrrr <kunotomoki61@gmail.com>
2023-11-06 16:46:00 +0900

    必要ないスクリプト

--- scripts/iASL_launch_psim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./vars/vehicle.env
-
-ros2 launch autoware_launch planning_simulator.launch.xml map_path:=../map/AWSIM/ vehicle_model:=$VEHICLE_MODEL sensor_model:=awsim_sensor_kit
